af38b69f64a00331c868635b1eb5f13d786fe33e
Payal <payalmohnani2003@gmail.com>
2023-03-11 12:31:13 +0530

    Database URL changed from postgresql to postgres

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from .config import settings
 # import time
 # from psycopg2.extras import RealDictCursor
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgres://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
