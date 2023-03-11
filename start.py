0d3227348c854ee8e98a39353085f461839167c4
Payal <payalmohnani2003@gmail.com>
2023-03-12 03:01:17 +0530

    removed database command from start.py

--- start.py
+++ start.py
@@ -2,4 +2,4 @@ import subprocess
 
 
 subprocess.run(['alembic', 'upgrade', 'head'])
-subprocess.run(['uvicorn', 'app.main:app', '--host', '0.0.0.0', '--port', '10000'])
+# subprocess.run(['uvicorn', 'app.main:app', '--host', '0.0.0.0', '--port', '10000'])
