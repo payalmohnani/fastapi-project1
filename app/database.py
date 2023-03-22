64cc0acc1f9806e5ceb70bc6849ee503ff6e66cd
Payal <payalmohnani2003@gmail.com>
2023-03-22 13:18:33 +0530

    Removed unnecessary commented code

--- app/database.py
+++ app/database.py
@@ -2,9 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings 
-# import psycopg2
-# import time
-# from psycopg2.extras import RealDictCursor
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
@@ -22,13 +19,3 @@ def get_db():
     finally:
         db.close()
 
-# Connect Database with our code using SQL
-# while True:
-#     try:
-#         conn = psycopg2.connect(host='localhost', database='fastapi',user='postgres',password='okapranam2k23',cursor_factory=RealDictCursor)
-#         cursor = conn.cursor()
-#         break
-#     except Exception as error:
-#         print('Connecting to database failed')
-#         print('Error: ',error)
-#         time.sleep(2)
--- app/routers/posts.py
+++ app/routers/posts.py
@@ -23,8 +23,6 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
 # get all posts (Read)
 @router.get("", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user), limit:int = 10, skip: int = 0, search: Optional[str]=""):
-    # cursor.execute("""SELECT * FROM posts """) 
-    # posts = cursor.fetchall()
     
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id,models.Post.title.contains(search)).limit(limit).offset(skip).all()
 
@@ -35,8 +33,7 @@ def get_posts(db: Session = Depends(get_db), current_user: models.User = Depends
 # get a particular post by id
 @router.get("/{id}", response_model=schemas.PostOut) # id represents path parameter
 def get_post(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user) ):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""",(str(id),))
-    # post = cursor.fetchone()
+    
     post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.id == id ).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -48,9 +45,6 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: models.User =
 @router.put("/{id}", status_code = status.HTTP_202_ACCEPTED, response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
 
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",(post.title, post.content, post.published, (str(id),)) )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
@@ -65,35 +59,11 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     db.commit()
 
     return post
-    
-
-# update a post partially
-# @app.patch("/posts/{id}", status_code = status.HTTP_202_ACCEPTED)
-# def patch_post(id: int, post: Post):
-# 
-    # cursor.execute("""UPDATE posts SET WHERE id = %s""",id)
-
-    # if id == None:
-    #     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-    #                         detail = f"Post with id {id} does not exist.")
-
-    # post_dict = post.dict()
-    # print(post_dict)
-    # for key,value in post_dict.items():
-    #     # if the value is updated
-    #     if value != None:
-    #         my_posts[index].update({key:value})
-    
-    # # print(my_posts)
-    # return {"message": "patched"}
-        
+            
 
 # delete a post
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""",(str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
