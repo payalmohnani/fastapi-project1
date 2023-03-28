4a53d99a0de8b9f8425fbdba494a905c61fc13ef
Payal <payalmohnani2003@gmail.com>
2023-03-29 02:56:46 +0530

    Error in Users file rectified

--- app/routers/users.py
+++ app/routers/users.py
@@ -13,10 +13,10 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     check_user = db.query(models.User).filter(models.User.email == new_user.email).first()
 
     if check_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"USer with email id: '{new_user.email}' already exists.\n Login instead or try with another Email ID")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"User with email id: '{new_user.email}' already exists.\n Login instead or try with another Email ID")
 
     # hash user given password and add user in db
-    user.password = utils.hash(user.password)
+    new_user.password = utils.hash(user.password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
